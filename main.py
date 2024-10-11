8b9efe5246a3c040727784d069b644e46576c055
chiefpat450119 <91508703+chiefpat450119@users.noreply.github.com>
2024-10-11 01:05:08 -0700

    More refactoring

--- main.py
+++ main.py
@@ -1,64 +1,18 @@
+import json
+import backoff
 from prawcore.exceptions import Forbidden, TooManyRequests, NotFound
 from praw.exceptions import RedditAPIException
 from reply import send_correction, bot_reply, check_feedback
 from mistake_db import mistakes
-import json
-import backoff
 from praw_setup import get_reddit
 from subreddits import get_subreddits, update_subreddits
 
 # Script will run every 3 hours and go through every subreddit in the list
 
-
-# Update total runs in stats file in case there's no change in mistake counter so no error is thrown
-def update_runs():
-    with open("data/stats.json", "r") as file:
-        data = json.load(file)
-        runs = int(data["total runs"])
-    runs += 1
-    with open("data/stats.json", "w") as file:
-        data["total runs"] = runs
-        json.dump(data, file)
-
-monitored_subreddits = []
-
-# Makes sure the comment author is not another bot
-def is_bot(comment):
-    try:
-        return "bot" in comment.author.name.lower()
-    except AttributeError:
-        return True
-
 @backoff.on_exception(backoff.expo, TooManyRequests, max_tries=10, raise_on_giveup=False)
-def main_loop(reddit):
-    # Reply to messages
-    for message in reddit.inbox.unread():
-        try:
-            # Check for STOP command
-            if "stop" in message.body.lower():
-                message.mark_read()
-                # Send a DM
-                reddit.redditor(message.author.name).message(subject="Bot Stopped",
-                                                             message="You will no longer receive corrections from the bot.")
-                # Add user to blocklist
-                with open("data/stopped_users.txt", "a") as f:
-                    f.write(f"{message.author.name}\n")
-
-            # Reply to any bots messages in the inbox
-            bot_reply(message)
-
-            # Check for feedback in comments
-            check_feedback(message)
-
-        except Forbidden:
-            continue
-        except AttributeError:
-            continue
-        except RedditAPIException:
-            continue
-
+def main_loop(reddit, subreddits):
     # Iterate through subreddits
-    for subreddit_name in monitored_subreddits:
+    for subreddit_name in subreddits:
         subreddit = reddit.subreddit(subreddit_name)
 
         # Iterate through submissions in hot
@@ -71,14 +25,7 @@ def main_loop(reddit):
                         # print(f"Checking comment {comment.id} in {subreddit.display_name}")
 
                         # Check conditions before replying
-                        # TODO: Refactor this into a separate function
-                        # Check if the user is on the blocklist
-                        with open("data/stopped_users.txt", "r") as f:
-                            stopped_users = f.read().splitlines()
-                        try:
-                            user_stopped = comment.author.name in stopped_users
-                        except AttributeError:
-                            user_stopped = False
+                        user_stopped = is_stopped(comment)
 
                         # Continue with check if all conditions met
                         if not any([is_bot(comment), comment.saved, user_stopped]):
@@ -118,13 +65,75 @@ def main_loop(reddit):
         except NotFound:
             continue
 
+
+def is_stopped(comment):
+    # Check if the user is on the blocklist
+    stopped_users = get_stopped_users()
+    try:
+        user_stopped = comment.author.name in stopped_users
+    except AttributeError:
+        user_stopped = False
+    return user_stopped
+
+# Update total runs in stats file in case there's no change in mistake counter so no error is thrown
+def update_runs():
+    with open("data/stats.json", "r") as file:
+        data = json.load(file)
+        runs = int(data["total runs"])
+    runs += 1
+    with open("data/stats.json", "w") as file:
+        data["total runs"] = runs
+        json.dump(data, file)
+
+# Makes sure the comment author is not another bot
+def is_bot(comment):
+    try:
+        return "bot" in comment.author.name.lower()
+    except AttributeError:
+        return True
+
+
+def get_stopped_users():
+    with open("data/stopped_users.txt", "r") as f:
+        stopped_users = f.read().splitlines()
+    return stopped_users
+
+def check_inbox(reddit):
+    # Reply to messages
+    for message in reddit.inbox.unread():
+        try:
+            # Check for STOP command
+            if "stop" in message.body.lower():
+                message.mark_read()
+                # Send a DM
+                reddit.redditor(message.author.name).message(subject="Bot Stopped",
+                                                             message="You will no longer receive corrections from the bot.")
+                # Add user to blocklist
+                with open("data/stopped_users.txt", "a") as f:
+                    f.write(f"{message.author.name}\n")
+
+            # Reply to any bots messages in the inbox
+            bot_reply(message)
+
+            # Check for feedback in comments
+            check_feedback(message)
+
+        except Forbidden:
+            continue
+        except AttributeError:
+            continue
+        except RedditAPIException:
+            continue
+
 if __name__ == "__main__":
     # Execute main loop
     try:
+        praw_instance = get_reddit()
+        check_inbox(reddit=praw_instance)
         # Update subreddit list
-        update_subreddits(reddit=get_reddit())
+        update_subreddits(reddit=praw_instance)
         monitored_subreddits = get_subreddits()
-        main_loop(reddit=get_reddit())
+        main_loop(reddit=praw_instance, subreddits=monitored_subreddits)
     # Catch rate limits
     except RedditAPIException as e:
         print(e)
